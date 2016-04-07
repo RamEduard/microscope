d1a472ca1d937168c0b0971ed86508ca25f436e2
Ramon Serrano <ramon.calle.88@gmail.com>
2016-04-07 12:26:12 -0430

    Updated to meteor 1.3.1.

--- client/templates/posts/post_new.js
+++ client/templates/posts/post_new.js
@@ -1,26 +1,3 @@
-Template.postNew.events({
-    'submit form': function(e) {
-        e.preventDefault();
-
-        var post = {
-            url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val()
-        };
-
-        Meteor.call('postInsert', post, function(error, result) {
-            // display error to the user and abort
-            if (error)
-                return $.notify(error.reason, 'error');
-            else if (result.postExists)
-                return $.notify('Post already exists!', 'warn');
-            else
-                $.notify('Post created successfully!', 'success');
-
-            Router.go('home');
-        });
-    }
-});
-
 Template.postNew.rendered = function () {
     $('button', '.note-editor').tooltip();
 };
